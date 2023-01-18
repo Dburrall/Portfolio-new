b9d9f79f365e85e840b579a3f2b27a6d7549901e
Dburrall <danburrall@gmail.com>
2023-01-18 11:29:26 -0500

    finished auto-text

--- text.js
+++ text.js
@@ -1,15 +1,6 @@
-const text = "Also I can make neat things"
-
-let index = 0;
-
-function writeText() {
-   document.getElementsByClassName("jstext") = text.slice(0, index);
-  
-  index++
-  
-  if (index > text.length - 1){
-    index= 0;
-  }
-}
-
-setInterval(writeText, 100);
+var typed = new Typed(".auto-text",{
+    strings:["Code", "program","Solve Problems"],
+    typeSpeed: 70,
+    backSpeed: 50,
+    loop:true
+})
