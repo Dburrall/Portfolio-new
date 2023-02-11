f16e8c20466090dbe4535e7ed45422f9844e3ede
Dburrall <danburrall@gmail.com>
2023-02-11 12:11:11 -0500

    getting cards set for websites

--- slidein.js
+++ slidein.js
@@ -1,17 +1,41 @@
-function reveal() {
-    var reveals = document.querySelectorAll("profile-pic");
+/*Interactivity to determine when an animated element in in view. In view elements trigger our animation*/
+$(document).ready(function() {
+
+    //window and animation items
+    var animation_elements = $.find('about');
+    var web_window = $(window);
   
-    for (var i = 0; i < reveals.length; i++) {
-      var windowHeight = window.innerHeight;
-      var elementTop = reveals[i].getBoundingClientRect().top;
-      var elementVisible = 150;
+    //check to see if any animation containers are currently in view
+    function check_if_in_view() {
+      //get current window information
+      var window_height = web_window.height();
+      var window_top_position = web_window.scrollTop();
+      var window_bottom_position = (window_top_position + window_height);
   
-      if (elementTop < windowHeight - elementVisible) {
-        reveals[i].classList.add("active");
-      } else {
-        reveals[i].classList.remove("active");
-      }
+      //iterate through elements to see if its in view
+      $.each(animation_elements, function() {
+  
+        //get the element sinformation
+        var element = $(this);
+        var element_height = $(element).outerHeight();
+        var element_top_position = $(element).offset().top;
+        var element_bottom_position = (element_top_position + element_height);
+  
+        //check to see if this current container is visible (its viewable if it exists between the viewable space of the viewport)
+        if ((element_bottom_position >= window_top_position) && (element_top_position <= window_bottom_position)) {
+          element.addClass('profile-pic');
+        } else {
+          element.removeClass('profile-pic');
+        }
+      });
+  
     }
-  }
   
-  window.addEventListener("scroll", reveal);
+    //on or scroll, detect elements in view
+    $(window).on('scroll resize', function() {
+        check_if_in_view()
+      })
+      //trigger our scroll event on initial load
+    $(window).trigger('scroll');
+  
+  });
