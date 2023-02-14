57a7e6f3b287b536548681e9591d5cfcfa23c0f0
Dburrall <danburrall@gmail.com>
2023-02-14 18:20:12 -0500

    contact section content added

--- text.js
+++ text.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".auto-text",{
-    strings:["Code", "Program","Solve Problems"],
+    strings:["Code", "Program","Game","Read"],
     typeSpeed: 70,
     backSpeed: 50,
     loop:true
