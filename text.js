bf8e24a151718e3972a45b42c25463d410f62bde
Dburrall <danburrall@gmail.com>
2023-02-17 19:13:07 -0500

    changed color and text sizes

--- text.js
+++ text.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".auto-text",{
-    strings:["Code", "Program","Game","Read"],
+    strings:["Code", "Learn","Game","Read"],
     typeSpeed: 70,
     backSpeed: 50,
     loop:true
